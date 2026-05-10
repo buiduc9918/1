0d2703e87b836d43db7ccbbdb7ee658e13e61371
buiduc9918 <buiduc9918>
2026-05-11 00:12:04 +0700

    ver4

--- app/src/main/java/com/example/myapplication/LoginFragment.java
+++ app/src/main/java/com/example/myapplication/LoginFragment.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.myapplication.databinding.FragmentLoginBinding;
 
@@ -26,10 +28,17 @@ public class LoginFragment extends Fragment {
         binding.btnTieptuc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               var number = binding.userNumber.toString();
-               if(number.length() == 0||number.length() !=10){
-
-               }
+                // Lấy text từ EditText bên trong TextInputLayout
+                String number = binding.userNumber.getEditText().getText().toString().trim();
+                
+                // Kiểm tra số điện thoại (Ví dụ: phải đúng 10 số)
+                if (number.length() != 10) {
+                    Toast.makeText(requireContext(), "Vui lòng nhập đúng số điện thoại", Toast.LENGTH_SHORT).show();
+                } else {
+                    Bundle bundle = new Bundle();
+                    bundle.putString("so", number);
+                    Navigation.findNavController(v).navigate(R.id.action_loginFragment2_to_OTPFragment2, bundle);
+                }
             }
         });
 
--- app/src/main/java/com/example/myapplication/OTPFragment.java
+++ app/src/main/java/com/example/myapplication/OTPFragment.java
@@ -8,14 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link OTPFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.myapplication.databinding.FragmentOTPBinding;
+
 public class OTPFragment extends Fragment {
     public OTPFragment() {
     }
+  private FragmentOTPBinding blinding;
     public static OTPFragment newInstance(String param1, String param2) {
         OTPFragment fragment = new OTPFragment();
         Bundle args = new Bundle();
@@ -29,7 +27,10 @@ public class OTPFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        blinding = FragmentOTPBinding.inflate(inflater,container,false);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_o_t_p, container, false);
+        return blinding.getRoot();
     }
+
 }
