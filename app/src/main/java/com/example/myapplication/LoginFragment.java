516de5988b1758cb1458e97b0ae076f6967f707b
buiduc9918 <buiduc9918>
2026-05-10 16:31:03 +0700

    second commit

--- app/src/main/java/com/example/myapplication/LoginFragment.java
+++ app/src/main/java/com/example/myapplication/LoginFragment.java
@@ -2,19 +2,40 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.myapplication.databinding.FragmentLoginBinding;
 
 public class LoginFragment extends Fragment {
+    private FragmentLoginBinding binding;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        // Sử dụng ViewBinding để inflate layout
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+
+        // Khai báo sự kiện setOnClickListener cho button "Tiếp"
+        binding.btnTieptuc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               var number = binding.userNumber.toString();
+               if(number.length() == 0||number.length() !=10){
+
+               }
+            }
+        });
+
+        return binding.getRoot();
     }
+//    // Chuyển sang OTPFragment khi nhấn nút trong onClick
+//                NavHostFragment.findNavController(LoginFragment.this)
+//            .navigate(R.id.action_loginFragment2_to_OTPFragment2);
 }
